3658fefbe5ac92bb2231b55212318401f1e4235f
ronggothelast <ronggothelast@gmail.com>
2025-08-05 22:06:33 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./index.html"], // sesuaikan dengan file HTML kamu
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        mono: ["JetBrains Mono", "monospace"], // opsional jika kamu butuh mono juga
+      },
+    },
+  },
+  plugins: [],
+};
+1;
